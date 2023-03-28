a3a13ea82d36aa7b6c0e5caab04fd8dc1a672a4a
mariam.mahareeq <mariam.mahareeq@transcend.ps>
2023-03-29 02:02:12 +0300

    config production mode

--- config/db.js
+++ config/db.js
@@ -1,30 +1,45 @@
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require("connect-mongodb-session")(session);
 const dotenv = require('dotenv');
 dotenv.config();
 mongoose.set("strictQuery", false);
 
-const connectDb = async ()=>{
-const host = process.env.MONGO_HOST || 'localhost';
-const port = parseInt(process.env.MONGO_PORT, 10) || 27017;
-const username = process.env.MONGO_USER || '';
-const password = process.env.MONGO_PWD || '';
-const db = process.env.MONGO_DB || 'tasteat-app';
+const databaseURI = () => {
+    let databaseUrl = '';
+    if(process.env.NODE_ENV === 'production'){
+        databaseUrl =process.env.DATA_BASE_URL;
+    }else{
+        const host = process.env.MONGO_HOST || 'localhost';
+    const port = parseInt(process.env.MONGO_PORT, 10) || 27017;
+    const username = process.env.MONGO_USER || '';
+    const password = process.env.MONGO_PWD || '';
+    const db = process.env.MONGO_DB || 'tasteat-app';
 
-const connectionArgs = ['mongodb://'];
+    const connectionArgs = ['mongodb://'];
 
-if (username && password) {
-    connectionArgs.push(`${username}:${password}@`);
+    if (username && password) {
+        connectionArgs.push(`${username}:${password}@`);
+    }
+
+    connectionArgs.push(`${host}:${port}/${db}`);
+    databaseUrl = connectionArgs.join('');
+    }
+    
+    return databaseUrl;
 }
 
-connectionArgs.push(`${host}:${port}/${db}`);
-const databaseUrl = connectionArgs.join('');
+
+const connectDb = async () => {
+
+    const databaseUrl = databaseURI();
     try {
         await mongoose.connect(databaseUrl);
-          console.log("DB Connection Successfull!");
+        console.log("DB Connection Successfull!");
     } catch (error) {
         console.log(error);
-        
+
     }
 }
 
-module.exports = connectDb;
+module.exports = {connectDb};
