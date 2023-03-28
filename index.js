c9d9a0f46e91a86d7252a9b2ce349bf8c38b7962
mariam.mahareeq <mariam.mahareeq@transcend.ps>
2023-03-29 02:03:00 +0300

    config production mode

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const errorHandler = require('./middleware/errorHandler');
-const connectDb = require('./config/db');
+const {connectDb} = require('./config/db');
 const bodyParser = require('body-parser');
 
 
